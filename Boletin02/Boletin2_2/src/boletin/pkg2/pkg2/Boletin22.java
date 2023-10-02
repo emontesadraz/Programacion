fcd60fa0f2761767968d344193f53718ac5267fd
DAM1 <emontesadraz@danielcastelao.org>
2023-10-02 09:23:16 +0200

    boletin22

---
+++ Boletin02/Boletin2_2/src/boletin/pkg2/pkg2/Boletin22.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletin.pkg2.pkg2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float base= 4,altura= 3;
+        float area= base*altura/2;
+        System.out.println("area do triangulo=" +area);
+    }
+    
+}
